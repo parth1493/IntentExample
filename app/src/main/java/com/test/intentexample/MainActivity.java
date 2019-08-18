2bca90d666f241cc7b2270aa3e1d370d59166eda
Parth <parth1493@gmail.com>
2019-08-18 00:46:03 -0400

    Implict intent

--- app/src/main/java/com/test/intentexample/MainActivity.java
+++ app/src/main/java/com/test/intentexample/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private EditText textInputLayout;
-    private Button button;
+    private Button button, button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,19 +19,43 @@ public class MainActivity extends AppCompatActivity {
 
         textInputLayout = findViewById(R.id.editText2);
         button  = findViewById(R.id.button);
+        button1  = findViewById(R.id.button2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-
-                Bundle b = new Bundle();
-                b.putString(Contants.CONT_NAME, textInputLayout.getText().toString());
-                b.putInt(Contants.CONT_AGE,23);
-                intent.putExtras(b);
-                startActivity(intent);
+                moveToSecondActivity_Firstway();
             }
         });
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToSecondActivity_Secondway();
+            }
+        });
     }
+
+    private void moveToSecondActivity_Firstway()
+    {
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+        Bundle b = new Bundle();
+        b.putString(Contants.CONT_NAME, textInputLayout.getText().toString());
+        b.putInt(Contants.CONT_AGE,23);
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
+    private void moveToSecondActivity_Secondway()
+    {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+        intent.putExtra(Contants.CONT_NAME,textInputLayout.getText().toString());
+        intent.putExtra(Contants.CONT_AGE,23);
+
+        startActivity(intent);
+    }
+
 }
